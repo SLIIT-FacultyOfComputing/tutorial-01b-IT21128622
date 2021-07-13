495e4fccd54d0a98a38137432ed5917fe09892c4
IT21128622 <87374252+IT21128622@users.noreply.github.com>
2021-07-13 18:46:39 +0000

    completed

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,18 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+  int dis , amo=0;
+
+  printf("Enter the travelled distance: ");
+  scanf("%d",&dis);
+
+  if(dis<=30){
+    amo=dis*50;
+  }
+  else{
+    amo=30*50+ (dis-30)*40;
+  }
+  printf("Total amount is %d",amo);
   
   return 0;
 }
