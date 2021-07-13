e18f8bab9176ec514eab657fada86e90806e8fd4
IT21128622 <87374252+IT21128622@users.noreply.github.com>
2021-07-13 13:11:03 +0000

    completed

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+  int x ,y,sum=0;
+  float avg=0;
+
+  printf("Enter num 1: ");
+  scanf("%d",& x);
+   printf("Enter num 2: ");
+  scanf("%d",& y); 
+
+  sum= x+y;
+  avg=sum/2.0;
+
+  printf("averge is :%.2f",avg);
+  
   
   return 0;
 }
